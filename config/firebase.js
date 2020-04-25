dd7dbddcf38474f9b70cadd99b22a901548f1621
Felipe Morato <felipecesar.morato@gmail.com>
2020-04-25 15:04:58 -0300

    adicionando files

---
+++ config/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD1Gy2T7rie2Ezdur_7gHC5En5-azcvg84",
+    authDomain: "aulanode.firebaseapp.com",
+    databaseURL: "https://aulanode.firebaseio.com",
+    projectId: "aulanode",
+    storageBucket: "aulanode.appspot.com",
+    messagingSenderId: "797339633043",
+    appId: "1:797339633043:web:58ad9d5b5ec10dfbc89466"
+  };
+
+  module.exports = firebaseConfig;
